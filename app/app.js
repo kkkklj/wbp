091fc2f5e6e9f37f245fd86d5455ec6fd09e7a0a
kkkklj <819103524@qq.com>
2019-11-15 15:31:36 +0800

    webpackDemo

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 require('../less/app.less');
+let img=require('../img/111.jpg');
+console.log('img==>',img);
 window.onload=function(){
-  let person=['a','b','c'];
+  let person=['a','b','c','d'];
   let ul=document.createElement('ul');
   person.map(
     (v,i,o)=>{
@@ -10,4 +12,12 @@ window.onload=function(){
     }
   )
   document.body.appendChild(ul);
+  let lis=document.getElementById('ul').children;
+  console.log(lis);
+  for(let i=0;i<lis.length;i++){
+    lis[i].onclick=function(){
+      console.log('i===>',i);
+      console.log(this.textContent);
+    }
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 //node.js的一个核心模块
 let path=require('path');
-let miniCssExtractPlugin=require('mini-css-extract-plugin')
+let miniCssExtractPlugin=require('mini-css-extract-plugin');
+let htmlWebpackPlugin=require('html-webpack-plugin');
 module.exports={
   mode:"development",
   entry:{
@@ -28,12 +29,41 @@ module.exports={
           {loader:'css-loader'},
           {loader:'less-loader'}
         ]
+      },
+      {
+        test:/\.(png|gif|jpg|jpeg)$/,
+        use:[
+          {
+            loader:'url-loader',
+            options:{
+              limit:1000
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html?$/,
+        use:[
+          {loader:'html-withimg-loader'}
+        ]
       }
+
     ]
+
   },
   plugins:[
     new miniCssExtractPlugin({
       filename: '[name].min.css'
+    }),
+    new htmlWebpackPlugin({
+      template:'./index.html',
+      inject:'body',
+      minify:{
+        removeComments:true,
+        removeAttributeQuotes:true,
+        collapseWhitespace:true
+      },
+      filename:'ohho.min.html'
     })
   ]
 }
